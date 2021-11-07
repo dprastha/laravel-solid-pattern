0005fe78bbcdf66a32d1b0200015a1403fe9d0fb
dprastha <danielprastha4@gmail.com>
2021-11-07 17:07:22 +0700

    implement laravel sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\v1\AuthController;
 use App\Http\Controllers\Api\v1\CommentController;
 use App\Http\Controllers\Api\v1\PostController;
 use App\Http\Controllers\Api\v1\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResources([
-    'users' => UserController::class,
-    'posts' => PostController::class,
-    'comments' => CommentController::class
-]);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResources([
+        'users' => UserController::class,
+        'posts' => PostController::class,
+        'comments' => CommentController::class
+    ]);
+});
+
+Route::post('login', [AuthController::class, 'login']);
